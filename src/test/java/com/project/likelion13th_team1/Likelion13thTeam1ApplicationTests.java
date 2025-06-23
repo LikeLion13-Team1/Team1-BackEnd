e72ffceb0d256ea2f15e3c0d6e2a415c69c431dc
Gyueon Lee <gon2yq@naver.com>
2025-06-23 15:33:15 +0900

    🎉Begin a Project

---
+++ src/test/java/com/project/likelion13th_team1/Likelion13thTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.likelion13th_team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion13thTeam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
