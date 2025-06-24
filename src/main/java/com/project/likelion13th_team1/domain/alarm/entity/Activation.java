a44c942fa1f8f685221b39095a2a575dbafe2e03
Gyueon Lee <gon2yq@naver.com>
2025-06-24 19:48:19 +0900

    ✨feat: Alarm 엔티티 구현

---
+++ src/main/java/com/project/likelion13th_team1/domain/alarm/entity/Activation.java
@@ -0,0 +1,5 @@
+package com.project.likelion13th_team1.domain.alarm.entity;
+
+public enum Activation {
+    Y, N
+}
--- src/main/java/com/project/likelion13th_team1/domain/alarm/entity/Alarm.java
+++ src/main/java/com/project/likelion13th_team1/domain/alarm/entity/Alarm.java
@@ -1,7 +1,10 @@
 package com.project.likelion13th_team1.domain.alarm.entity;
 
+import com.project.likelion13th_team1.domain.routine.entity.RoutineEvent;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -10,4 +13,21 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "alarm")
 public class Alarm{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "context", nullable = false)
+    private String context;
+
+    @Column(name = "activation", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Activation activation;
+
+    @Column(name = "time", nullable = false)
+    private LocalDateTime time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private RoutineEvent routineEvent;
 }
