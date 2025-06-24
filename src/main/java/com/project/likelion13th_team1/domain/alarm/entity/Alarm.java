e61616776c0fec88e8d75330f5de2694c0a783dc
Gyueon Lee <gon2yq@naver.com>
2025-06-24 19:58:04 +0900

    🚀chore: Alarm 코드 주석 추가

--- src/main/java/com/project/likelion13th_team1/domain/alarm/entity/Alarm.java
+++ src/main/java/com/project/likelion13th_team1/domain/alarm/entity/Alarm.java
@@ -14,20 +14,25 @@ import java.time.LocalDateTime;
 @Table(name = "alarm")
 public class Alarm{
 
+    // PK
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 알람 내용
     @Column(name = "context", nullable = false)
     private String context;
 
+    // 할거냐 말거냐
     @Column(name = "activation", nullable = false)
     @Enumerated(EnumType.STRING)
     private Activation activation;
 
+    // 알람을 할 시간
     @Column(name = "time", nullable = false)
     private LocalDateTime time;
 
+    // 알람을 할 실제 루틴 정보
     @ManyToOne(fetch = FetchType.LAZY)
     private RoutineEvent routineEvent;
 }
