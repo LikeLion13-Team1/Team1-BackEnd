d5ce8a0069a7a0e4b93e46ea53c352a84a2c53ea
Gyueon Lee <gon2yq@naver.com>
2025-06-24 19:48:32 +0900

    ✨feat: Membership 엔티티 구현

--- src/main/java/com/project/likelion13th_team1/domain/membership/entity/Membership.java
+++ src/main/java/com/project/likelion13th_team1/domain/membership/entity/Membership.java
@@ -1,8 +1,10 @@
 package com.project.likelion13th_team1.domain.membership.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.project.likelion13th_team1.domain.member.entity.Member;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -11,4 +13,22 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "membership")
 public class Membership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MembershipStatus status;
+
+    @Column(name = "startAt", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "expiredAt", nullable = false)
+    private LocalDateTime expiredAt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
---
+++ src/main/java/com/project/likelion13th_team1/domain/membership/entity/MembershipStatus.java
@@ -0,0 +1,5 @@
+package com.project.likelion13th_team1.domain.membership.entity;
+
+public enum MembershipStatus {
+    STANDARD, PRO
+}
