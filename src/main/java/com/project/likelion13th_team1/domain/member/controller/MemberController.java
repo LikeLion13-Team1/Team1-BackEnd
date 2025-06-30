a6df6b4c7318e4a203a9545ffdf8ff05304d807e
Gyueon Lee <gon2yq@naver.com>
2025-06-30 15:32:37 +0900

    🚀chore: uri 수정

--- src/main/java/com/project/likelion13th_team1/domain/member/controller/MemberController.java
+++ src/main/java/com/project/likelion13th_team1/domain/member/controller/MemberController.java
@@ -7,21 +7,18 @@ import com.project.likelion13th_team1.global.apiPayload.CustomResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/member")
 @Tag(name = "Member", description = "멤버 관련 API")
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
 
     @Operation
-    @PostMapping("signup")
+    @PostMapping("/signup")
     public CustomResponse<MemberResponseDto.MemberCreateResponseDto> createMember(
             @RequestBody MemberRequestDto.MemberCreateRequestDto memberCreateRequestDto
     ) {
