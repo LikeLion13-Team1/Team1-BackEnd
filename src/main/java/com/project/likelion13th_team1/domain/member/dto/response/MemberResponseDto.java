6bc7cf3454303b841e902e37167b243281171b90
Gyueon Lee <gon2yq@naver.com>
2025-06-30 22:58:59 +0900

    🔥remove: 사용하지 않는 dto 제거

--- src/main/java/com/project/likelion13th_team1/domain/member/dto/response/MemberResponseDto.java
+++ src/main/java/com/project/likelion13th_team1/domain/member/dto/response/MemberResponseDto.java
@@ -1,21 +1,5 @@
 package com.project.likelion13th_team1.domain.member.dto.response;
 
-import lombok.Builder;
-
-import java.time.LocalDateTime;
-
 public class MemberResponseDto {
 
-    @Builder
-    public record MemberCreateResponseDto(
-            Long id,
-            LocalDateTime createdAt
-    ) {
-    }
-
-    @Builder
-    public record MemberUpdateResponseDto(
-            LocalDateTime updatedAt
-    ) {
-    }
 }
