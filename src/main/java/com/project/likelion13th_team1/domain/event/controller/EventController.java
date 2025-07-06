c2b464a3f57bb94c399cd0de8ec04abaf14e58f4
Gyueon Lee <gon2yq@naver.com>
2025-07-07 01:51:01 +0900

    🚀chore: Event API Swagger description

--- src/main/java/com/project/likelion13th_team1/domain/event/controller/EventController.java
+++ src/main/java/com/project/likelion13th_team1/domain/event/controller/EventController.java
@@ -8,6 +8,7 @@ import com.project.likelion13th_team1.global.apiPayload.CustomResponse;
 import com.project.likelion13th_team1.global.security.userdetails.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,7 @@ public class EventController {
     private final EventCommandService eventCommandService;
     private final EventQueryService eventQueryService;
 
-    @Operation(summary = "루틴 이벤트 단일 조회")
+    @Operation(summary = "루틴 이벤트 단일 조회", description = "이벤트 아이디로 이벤트 객체 1개를 검색한다.")
     @GetMapping("/{eventId}")
     public CustomResponse<EventResponseDto.EventDetailResponseDto> getEvent(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -34,7 +35,10 @@ public class EventController {
         return CustomResponse.onSuccess(eventQueryService.getEvent(customUserDetails.getUsername(), eventId));
     }
 
-    @Operation(summary = "루틴 이벤트 목록 커서 조회 (날짜 검색)")
+    @Operation(summary = "루틴 이벤트 목록 커서 조회 (날짜 검색)", description = "cursor은 커서 위치로 맨 초기에는 0을 입력한다<br>" +
+            "size는 한번에 나타낼 객체의 개수이다.<br>" +
+            "hasNextCursor가 true라면 뒤에 내용이 더 있다는 의미이므로 다음 커서를 nextCursor값으로 입력하면 계속해서 객체가 출력된다." +
+            "<br> 날짜를 기준으로 검색하므로 YYYY-MM-DDTHH-MM-SS 의 형태로 검색 시작 날짜와 끝 날짜를 입력한다.")
     @GetMapping()
     public CustomResponse<EventResponseDto.EventCursorResponseDto> getEventCursor(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -46,17 +50,17 @@ public class EventController {
         return CustomResponse.onSuccess(eventQueryService.getEventCursor(customUserDetails.getUsername(), cursor, size, start, end));
     }
 
-    @Operation(summary = "루틴 이벤트 수정")
+    @Operation(summary = "루틴 이벤트 수정", description = "이벤트가 수행될 시간 scheduledAt은 null 일 수 없다.")
     @PatchMapping("/{eventId}")
     public CustomResponse<EventResponseDto.EventUpdateResponseDto> updateEvent(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @PathVariable Long eventId,
-            @RequestBody EventRequestDto.EventUpdateRequestDto eventUpdateRequestDto
+            @RequestBody @Valid EventRequestDto.EventUpdateRequestDto eventUpdateRequestDto
     ) {
         return CustomResponse.onSuccess(eventCommandService.updateEvent(customUserDetails.getUsername(), eventId, eventUpdateRequestDto));
     }
 
-    @Operation(summary = "루틴 이벤트 삭제")
+    @Operation(summary = "루틴 이벤트 삭제", description = "이벤트 아이디를 통해 이벤트 객체를 삭제한다.")
     @DeleteMapping("/{eventId}")
     public CustomResponse<String> deleteEvent(
         @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -66,7 +70,7 @@ public class EventController {
         return CustomResponse.onSuccess(HttpStatus.NO_CONTENT, "루틴 이벤트 삭제 완료");
     }
 
-    @Operation(summary = "루틴 완료")
+    @Operation(summary = "루틴 완료", description = "루틴을 수행했을 때, 루틴 수행완료 시간을 현재 시간으로 설정하고, 해당 status를 SUCCESS로 변경한다.")
     @PatchMapping("/{eventId}/done")
     public CustomResponse<EventResponseDto.EventDoneResponseDto> doneEvent(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
@@ -75,7 +79,7 @@ public class EventController {
         return CustomResponse.onSuccess(eventCommandService.doneEvent(customUserDetails.getUsername(), eventId));
     }
 
-    @Operation(summary = "루틴 완료 취소")
+    @Operation(summary = "루틴 완료 취소", description = "수행한 루틴의 수행완료를 되돌리고 싶을 때, 루틴 수행완료 시간을 null로 설정하고, 해당 status를 PROCESSING으로 변경한다.")
     @PatchMapping("/{eventId}/undone")
     public CustomResponse<EventResponseDto.EventDoneResponseDto> undoneEvent(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
