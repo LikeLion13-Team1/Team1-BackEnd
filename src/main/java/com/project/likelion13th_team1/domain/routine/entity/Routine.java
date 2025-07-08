3c25066771c2339bd2ad6cc92094349823bd4fed
Gyueon Lee <gon2yq@naver.com>
2025-07-09 02:11:37 +0900

    ✨feat: 루틴 엔티티 수정

--- src/main/java/com/project/likelion13th_team1/domain/routine/entity/Routine.java
+++ src/main/java/com/project/likelion13th_team1/domain/routine/entity/Routine.java
@@ -32,15 +32,15 @@ public class Routine extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    // 루틴 상태 (진행 중인지 끝났는지)
-    @Column(name = "status")
-    @Enumerated(EnumType.STRING)
-    private Status status;
+//    // 루틴 상태 (진행 중인지 끝났는지)
+//    @Column(name = "status")
+//    @Enumerated(EnumType.STRING)
+//    private Status status;
 
-    // 루틴 타입 (유저가 만든 것인지, 추천받은 것인지)
-    @Column(name = "type", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Type type;
+//    // 루틴 타입 (유저가 만든 것인지, 추천받은 것인지)
+//    @Column(name = "type", nullable = false)
+//    @Enumerated(EnumType.STRING)
+//    private Type type;
 
     // 반복 주기
     @Column(name = "cycle", nullable = false)
@@ -59,27 +59,27 @@ public class Routine extends BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    // 멤버 FK
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+//    // 멤버 FK
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
 
     // 그룹 FK
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
 
-    // 루틴 특성 FK (유저에게 추천해주기 위한?)
-    // TODO : 근데 CUSTOM 으로 만드는 경우에는 어떻게 할지?
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "feature_id")
-    private Feature feature;
+//    // 루틴 특성 FK (유저에게 추천해주기 위한?)
+//    // TODO : 근데 CUSTOM 으로 만드는 경우에는 어떻게 할지?
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "feature_id")
+//    private Feature feature;
 
     // TODO : DTO 제약 조건이 필요하다
     public void updateRoutine(RoutineRequestDto.RoutineUpdateRequestDto dto) {
         if (dto.name() != null) this.name = dto.name();
         if (dto.description() != null) this.description = dto.description();
-        if (dto.status() != null) this.status = dto.status();
+//        if (dto.status() != null) this.status = dto.status();
         if (dto.cycle() != null) this.cycle = dto.cycle();
         if (dto.startAt() != null) this.startAt = dto.startAt();
         if (dto.endAt() != null) this.endAt = dto.endAt();
