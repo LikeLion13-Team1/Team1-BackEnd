c46bc4e8bf3b58b46cf7bcbb29cca4d23219f40f
port5862 <port5862@naver.com>
2025-07-12 00:58:35 +0900

    Update AlarmQueryService.java

--- src/main/java/com/project/likelion13th_team1/domain/alarm/service/query/AlarmQueryService.java
+++ src/main/java/com/project/likelion13th_team1/domain/alarm/service/query/AlarmQueryService.java
@@ -1,4 +1,15 @@
 package com.project.likelion13th_team1.domain.alarm.service.query;
 
+import com.project.likelion13th_team1.domain.alarm.dto.response.AlarmDto;
+import com.project.likelion13th_team1.domain.alarm.dto.response.AlarmResponseDto;
+import com.project.likelion13th_team1.domain.alarm.entity.Alarm;
+import com.project.likelion13th_team1.domain.member.entity.Member;
+import com.project.likelion13th_team1.global.security.userdetails.CustomUserDetails;
+
+import java.util.List;
+
 public interface AlarmQueryService {
+    AlarmResponseDto.AlarmDetailResponseDto getAlarms1(Long routineEventId, Long cursor, Integer size);
+    List<Alarm> getAlarms(CustomUserDetails customUserDetails);
+    List<AlarmDto> getAlarmsByMember(Member member);
 }
