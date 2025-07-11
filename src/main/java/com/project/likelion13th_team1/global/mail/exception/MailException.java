95d759478e5f535f33820333fa6df98b442e5dec
Gyueon Lee <gon2yq@naver.com>
2025-07-11 23:29:18 +0900

    test

--- src/main/java/com/project/likelion13th_team1/global/mail/exception/MailException.java
+++
@@ -1,9 +0,0 @@
-package com.project.likelion13th_team1.global.mail.exception;
-
-import com.project.likelion13th_team1.global.apiPayload.exception.CustomException;
-
-public class MailException extends CustomException {
-    public MailException(MailErrorCode errorCode) {
-        super(errorCode);
-    }
-}
