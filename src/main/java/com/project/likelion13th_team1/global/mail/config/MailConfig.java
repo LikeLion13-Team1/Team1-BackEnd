05101d024fd56ac207ad3af32fff5636be3b2f64
Gyueon Lee <gon2yq@naver.com>
2025-07-11 22:04:18 +0900

    ✨feat: MailConfig.java 추가

---
+++ src/main/java/com/project/likelion13th_team1/global/mail/config/MailConfig.java
@@ -0,0 +1,16 @@
+package com.project.likelion13th_team1.global.mail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
+
